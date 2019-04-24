6b98aaa1ed3deb0ba74900ecbe063641644a6921
luizmonteiro <luizmonteiro@NBBV017353.BRQ.COM>
2019-04-24 17:32:28 -0300

    calculando frete

--- src/main/java/com/les/ecommerce/helpers/pedido/PedidoFreteHelper.java
+++ src/main/java/com/les/ecommerce/helpers/pedido/PedidoFreteHelper.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 public class PedidoFreteHelper {
 
 	public Double calcularFreteByCep(String cep) {
-		
+		String lastDigits = cep.substring(cep.length() - 3);
+		Double CepDigits = 777D;
+		Double f = Double.parseDouble(lastDigits);
+		return Math.abs((f - CepDigits)) * (Math.random() * 20);
 	}
 }
